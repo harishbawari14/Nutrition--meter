75b18d7c59d0ad24d246455fecaf496bc0a03697
harishbawari14 <163107472+harishbawari14@users.noreply.github.com>
2025-02-25 19:35:34 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,14 @@
+//App.js 
+import React from "react"; 
+import NutritionMeter from "./NutritionMeter"; 
+
+function App() { 
+return ( 
+	<div className=" bg-gray-200 min-h-screen"> 
+	<NutritionMeter /> 
+	</div> 
+); 
+} 
+
+export default App; 
+
